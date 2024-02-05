e4ed5edb4e2e77d4fcb7f869547ddd67e8964c2a
Abdul H <riipon1@outlook.com>
2024-02-05 16:42:47 +0000

    add functionality and css style to html

--- index.js
+++ index.js
@@ -7,4 +7,44 @@ let player;
 let computer;
 let result;
 
-choiceBtns.forEach(button => button);
+choiceBtns.forEach(button => button.addEventlistener("click", ()=>{
+
+    player = button.textContent;
+    computerTurn();
+    playerText.textContent = `Player: ${player}`;
+    computerText.textContent = `Computer: ${computer}`
+    resultText.textContent = checkWinner();
+
+}));
+
+function computerTurn (){
+    const randNum = Math.floor(Math.random() * 3) + 1;
+    
+    switch(randNum){
+        case 1:
+            computer = "ROCK";
+            break;
+        case 2:
+            computer = "PAPER";
+            break;
+        case 3:
+            computer = "SCISSORS";
+            break;
+    }
+}
+
+
+function checkWinner (){
+    if (player==computer){
+      return "Draw";  
+    }
+    else if (computer == "ROCK"){
+        return (player == "PAPER");  "You Win!" ; "You Lose!"
+    }
+    else if (computer == "PAPER"){
+        return (player == "SCISSORS"); "You Win!" ; "You Lose!"
+    }
+    else if (computer == "SCISSORS"){
+        return (player == "ROCK"); "You Win!" ; "You Lose!"
+    }
+}
