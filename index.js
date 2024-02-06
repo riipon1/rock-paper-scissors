fa60d8aa9c811d912c03f568ae530093b28a6411
Abdul H <riipon1@outlook.com>
2024-02-06 15:51:25 +0000

    add functionality to the game

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
-const playerText = document.querySelector("#playerText");
-const computerText = document.querySelector("#computerText");
-const resultText = document.querySelector("#resultText");
-const choiceBtn = document.querySelector(".choiceBtn");
+//Rock Paper Scissors
+
+const choicesDiv = ["rock", "paper", "scissors"];
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const resultdisplay = document.getElementById("resultDisplay");
+
+function playGame(playerChoice){
+
+    const computerChoice = choicesDiv[Math.floor(Math.random() * 3)];
+
+    let result = "";
+
+    if (playerChoice === computerChoice){
+        result = "It's a Tie!"
+    }
+    else{
+        switch(playerChoice){
+            case "rock":
+              result = (computerChoice === "scissors") ? "You Win!" : "You Lose!";
+              break;
+            case "paper":
+              result = (computerChoice === "rock") ? "You Win!" : "You Lose!";
+              break;
+            case "scissors":
+              result = (computerChoice === "paper") ? "You Win!" : "You Lose!";
+              break;
+          }
+        }
+
+        
+        playerDisplay.textContent = `PLAYER: ${playerChoice}`
+        computerChoice.textContent = `Computer: ${computerChoice}`;
+        resultdisplay.textContent = result;
+    }
