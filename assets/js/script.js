f67f3cfb547f30d771f7fb36d025251a75a28764
Abdul H <121059658+riipon1@users.noreply.github.com>
2024-04-23 13:31:30 +0000

    add all const on script.js for fuctionality

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const computerImages = document.querySelectorAll(".computer img");
+    const playerImages = document.querySelectorAll(".player img");
+    const computerPoint = document.querySelector(".computerPoint");
+    const playerPoint = document.querySelector(".playerPoint");
+    const resultDisplay = document.querySelector(".resultDisplay");
+    const options = document.querySelectorAll(".options button");
+    const resetButton = document.querySelector(".reset-btn");
+
+
+
+});
